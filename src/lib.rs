3e85339c0530827a48ad7e8786d757eaca9ec820
Patrick Jackson <patrick@jackson.dev>
2024-05-15 18:56:18 -0700

    feat: add basic building support

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,79 @@ use rand::Rng;
 use std::collections::HashMap; // Add this line to import the `Rng` trait from the `rand` crate
 
 /// An Island in the world
-struct Island {}
+struct Island {
+    buildings: Vec<Box<dyn Building>>,
+}
+
+/// Define the interface for a building
+pub trait Building {
+    fn name(&self) -> &str;
+    fn level(&self) -> usize;
+    fn score(&self) -> usize {
+        self.level()
+    }
+}
+
+struct GoldPit {
+    level: usize,
+}
+
+impl GoldPit {
+    pub fn new(level: usize) -> Self {
+        Self { level }
+    }
+}
+
+impl Building for GoldPit {
+    fn name(&self) -> &str {
+        "goldpit"
+    }
+
+    fn level(&self) -> usize {
+        self.level
+    }
+}
 
 impl Island {
     /// Create a new island
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(config: &IslandConfig) -> Self {
+        let buildings = config
+            .buildings
+            .iter()
+            .map(|b| match b.name.as_str() {
+                "goldpit" => {
+                    Box::new(GoldPit::new(b.starting_level.unwrap_or(0))) as Box<dyn Building>
+                }
+                _ => panic!("Building not found"),
+            })
+            .collect();
+
+        Self { buildings }
     }
 
     /// Get the score of an island
     pub fn score(&self) -> usize {
-        0
+        self.buildings.iter().map(|b| b.score()).sum()
     }
+}
+
+/// Configuration for the creation of an island
+pub struct IslandConfig {
+    /// List of buildings that will be built on the island
+    pub buildings: Vec<BuildingConfig>,
+
+    /// Starting resources for the island
+    pub resources: HashMap<String, usize>,
+}
+
+pub struct BuildingConfig {
+    /// Name of the building
+    /// Must be one of the predefined types
+    pub name: String,
+
+    /// Starting level for this type of building
+    /// If not provided it is 0
+    pub starting_level: Option<usize>,
 }
 
 pub struct World {
@@ -28,6 +89,9 @@ pub struct WorldConfig {
 
     /// World size
     pub size: (usize, usize),
+
+    /// Island Config
+    pub islands: IslandConfig,
 }
 
 impl World {
@@ -42,7 +106,7 @@ impl World {
             let mut rng = rand::thread_rng();
             let x: usize = rng.gen_range(0..=config.size.0);
             let y: usize = rng.gen_range(0..=config.size.1);
-            let island = Island::new();
+            let island = Island::new(&config.islands);
             islands.insert((x, y), island);
         }
         Self { config, islands }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use islandfight::{World, WorldConfig};
+use islandfight::{BuildingConfig, IslandConfig, World, WorldConfig};
 
 fn main() {
     // For now, we're just running some very basic stuff here to test the lib
     let world = World::new(WorldConfig {
         island_count: 10,
         size: (100, 100),
+        islands: IslandConfig {
+            buildings: vec![BuildingConfig {
+                name: "goldpit".to_string(),
+                starting_level: None,
+            }],
+            resources: Default::default(),
+        },
     });
     println!("{}", world.stats());
 }
