cd28856a75e54cb74b1eab36b29419ba4cb493f7
Patrick Jackson <patrick@jackson.dev>
2024-05-15 18:58:27 -0700

    feat: add very basic events

--- src/lib.rs
+++ src/lib.rs
@@ -3,58 +3,113 @@ use std::collections::HashMap; // Add this line to import the `Rng` trait from t
 
 /// An Island in the world
 struct Island {
-    buildings: Vec<Box<dyn Building>>,
+    /// List of events that are happening on the island
+    events: Vec<Event>,
+    gold: usize,
+    _lumber: usize,
+    _stone: usize,
+    buildings: Buildings,
 }
 
-/// Define the interface for a building
-pub trait Building {
-    fn name(&self) -> &str;
-    fn level(&self) -> usize;
-    fn score(&self) -> usize {
-        self.level()
-    }
+struct Buildings {
+    goldpit: usize,
 }
 
-struct GoldPit {
-    level: usize,
+#[derive(Clone)]
+struct Event {
+    completion: usize,
+    callback: EventCallback,
 }
 
-impl GoldPit {
-    pub fn new(level: usize) -> Self {
-        Self { level }
-    }
-}
-
-impl Building for GoldPit {
-    fn name(&self) -> &str {
-        "goldpit"
-    }
-
-    fn level(&self) -> usize {
-        self.level
-    }
+#[derive(Clone)]
+enum EventCallback {
+    Gold,
 }
 
 impl Island {
     /// Create a new island
-    pub fn new(config: &IslandConfig) -> Self {
-        let buildings = config
-            .buildings
-            .iter()
-            .map(|b| match b.name.as_str() {
-                "goldpit" => {
-                    Box::new(GoldPit::new(b.starting_level.unwrap_or(0))) as Box<dyn Building>
-                }
-                _ => panic!("Building not found"),
-            })
-            .collect();
+    pub fn new(tick: usize, config: &IslandConfig) -> Self {
+        let gold = *config.resources.get("gold").unwrap_or(&0);
+        let lumber = *config.resources.get("lumber").unwrap_or(&0);
+        let stone = *config.resources.get("stone").unwrap_or(&0);
+        let events = vec![Event {
+            completion: tick + 10,
+            callback: EventCallback::Gold,
+        }];
+        Self {
+            events,
+            gold,
+            _lumber: lumber,
+            _stone: stone,
+            buildings: Buildings { goldpit: 0 },
+        }
+    }
 
-        Self { buildings }
+    /// Callback for the goldpit, increase the gold amount by one and create a new callback
+    fn goldpit_callback(
+        tick: usize,
+        _world_config: &WorldConfig,
+        island: &mut Island,
+    ) -> Option<Event> {
+        island.gold += 1;
+        Some(Event {
+            completion: tick + 10,
+            callback: EventCallback::Gold,
+        })
+    }
+
+    pub fn gold(&mut self, tick: usize, world_config: &WorldConfig) -> usize {
+        self.process_events(tick, world_config);
+        self.gold
+    }
+
+    /// Check if there is an event that needs to be processed
+    pub fn event_to_process(&mut self, tick: usize) -> bool {
+        // Check if the first event is ready to be processed
+        if let Some(event) = self.events.first() {
+            event.completion <= tick
+        } else {
+            false
+        }
+    }
+
+    /// Register a new event
+    /// The event will be sorted by the completion time
+    pub fn register_event(&mut self, event: Event) {
+        self.events.push(event);
+        self.events.sort_by_key(|e| e.completion)
+    }
+
+    /// Process all events that are expected to happen
+    pub fn process_events(&mut self, tick: usize, world_config: &WorldConfig) {
+        let mut dirty = true;
+        while dirty && self.event_to_process(tick) {
+            dirty = false;
+            let events = self.events.clone();
+            self.events.clear();
+            for event in events.iter() {
+                if event.completion <= tick {
+                    dirty = true;
+                    match event.callback {
+                        EventCallback::Gold => {
+                            if let Some(new_event) =
+                                Self::goldpit_callback(event.completion, world_config, self)
+                            {
+                                self.register_event(new_event);
+                            }
+                        }
+                    }
+                } else {
+                    self.register_event(event.clone());
+                }
+            }
+        }
     }
 
     /// Get the score of an island
-    pub fn score(&self) -> usize {
-        self.buildings.iter().map(|b| b.score()).sum()
+    pub fn score(&mut self, tick: usize, world_config: &WorldConfig) -> usize {
+        self.process_events(tick, world_config);
+        self.buildings.goldpit
     }
 }
 
@@ -98,7 +153,7 @@ impl World {
     /// Create a new World
     ///
     /// Uses a custom configuration struct to set up all the details
-    pub fn new(config: WorldConfig) -> Self {
+    pub fn new(config: WorldConfig, initial_tick: usize) -> Self {
         let mut islands = HashMap::new();
         for _ in 0..config.island_count {
             // Create a new island at a random location in the 2d space
@@ -106,20 +161,21 @@ impl World {
             let mut rng = rand::thread_rng();
             let x: usize = rng.gen_range(0..=config.size.0);
             let y: usize = rng.gen_range(0..=config.size.1);
-            let island = Island::new(&config.islands);
+            let island = Island::new(initial_tick, &config.islands);
             islands.insert((x, y), island);
         }
         Self { config, islands }
     }
 
     /// Return basic stats about the World
-    pub fn stats(&self) -> String {
+    pub fn stats(&mut self, tick: usize) -> String {
         let mut stats = format!("Island count: {}\n", self.config.island_count);
-        for (pos, island) in self.islands.iter() {
+        for (coords, island) in self.islands.iter_mut() {
             stats.push_str(&format!(
-                "Island at {:?} has score {}\n",
-                pos,
-                island.score()
+                "Island at {:?} has score {} and gold {}\n",
+                coords,
+                island.score(tick, &self.config),
+                island.gold(tick, &self.config),
             ));
         }
         stats
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use islandfight::{BuildingConfig, IslandConfig, World, WorldConfig};
+use islandfight::{IslandConfig, World, WorldConfig};
 
 fn main() {
     // For now, we're just running some very basic stuff here to test the lib
-    let world = World::new(WorldConfig {
-        island_count: 10,
-        size: (100, 100),
-        islands: IslandConfig {
-            buildings: vec![BuildingConfig {
-                name: "goldpit".to_string(),
-                starting_level: None,
-            }],
-            resources: Default::default(),
+    let mut world = World::new(
+        WorldConfig {
+            island_count: 2,
+            size: (100, 100),
+            islands: IslandConfig {
+                resources: Default::default(),
+                buildings: Default::default(),
+            },
         },
-    });
-    println!("{}", world.stats());
+        0,
+    );
+    println!("{}", world.stats(29));
+    println!("{}", world.stats(30));
+    println!("{}", world.stats(31));
 }
